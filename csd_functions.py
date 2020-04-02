571bdd8644824109534e10cbdb7d60a964134203
Nick Tolley <55253912+ntolley@users.noreply.github.com>
2020-04-02 10:52:24 -0400

    Succesful implementation of multiresolution reeb graph

--- csd_functions.py
+++ csd_functions.py
@@ -11,15 +11,11 @@ import quantities as pq
 import os
 from os.path import isfile, join
 from os import listdir
+import networkx as nx
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
     
-#Adjust scalars to center of each contour range, inserts nodes at edges that span range
-def resample_tree(node_data, edge_data, bounds):
-    return
-
-#Returns the minimum distance between two nodes
-def min_node_dist(node_data):
-    return
-
 
 #Takes in a .param dictionary, parameter sweeps defined by an array of values, generates all combinations 
 def dict_expand(array_dict):
@@ -165,3 +161,121 @@ def load_tree(prefix):
     node_color = np.array(csd_nodes_df[['CriticalType']])
 
     return node_points, node_connectivity, node_color
+
+def plot_graph(G):
+    new_points = np.array([G.nodes[node_idx]['Position'] for node_idx in list(G.nodes)])
+    new_connectivity = np.array([list(e) for e in list(G.edges())])
+
+    fig = plt.figure(figsize = (8,6))
+    ax = plt.axes(projection='3d')
+
+    num_pairs = new_connectivity.shape[0]
+    for pair in range(num_pairs):
+        pairID = new_connectivity[pair]
+        pos_start, pos_end = G.nodes[pairID[0]]['Position'], G.nodes[pairID[1]]['Position']
+        xdata, ydata, zdata = [pos_start[0], pos_end[0]], [pos_start[1], pos_end[1]], [pos_start[2], pos_end[2]]
+
+        ax.plot(xdata,ydata,zdata, 'k', linewidth=0.2)
+
+    ax.scatter(new_points[:, 0], new_points[:, 1], new_points[:, 2], 'b')
+
+    plt.show()
+    return
+
+# adds or removes nodes based on interval
+def edge_edit(B, start_node, end_node, interval_points):
+    # print(str([start_node,end_node]))
+
+    #Height of current node
+    start_val = B.nodes[start_node]['Position'][2]
+    end_val = B.nodes[end_node]['Position'][2]
+
+    #Upper bound index for the start and ending node heights
+    start_bound_idx, end_bound_idx = np.sum(interval_points < start_val), np.sum(interval_points < end_val)
+
+    #Update node positions to center of interval
+    B.nodes[start_node]['Position'][2] == np.mean(interval_points[start_bound_idx-1:start_bound_idx]) #*** Speed up with computing interval width ***
+    B.nodes[end_node]['Position'][2] == np.mean(interval_points[end_bound_idx-1:end_bound_idx])
+
+    #Represents how many intervals the edge spans
+    bound_diff = abs(start_bound_idx - end_bound_idx)
+
+    #Edge spans exactly 1 bound as desired, update node position and return
+    if bound_diff == 1:
+        print(str([start_node,end_node]), 'skip')
+
+        return None
+
+    #Connected nodes in same interval, merge together and update attribute dictionary
+    elif bound_diff == 0:
+        print(str([start_node,end_node]), 'merge')
+
+        #Update attribute to indicate merging
+        B.nodes[start_node]['Merged'].append(end_node)
+
+        #Join end node neighbors to start node 
+        merged_neighbors = list(B.neighbors(end_node))
+        merged_neighbors.remove(start_node)
+        B.remove_node(end_node)
+
+        new_edges = [[start_node, new_neighbor] for new_neighbor in merged_neighbors]
+        B.add_edges_from(new_edges)
+
+        #update neighbors graph_search by reference
+
+        return merged_neighbors
+
+    #Edge spans more than one interval, subdivide
+    elif bound_diff > 1:
+        print(str([start_node,end_node]), 'insert')
+
+        B.remove_edge(start_node, end_node)
+        
+        #Create new nodes with unique id's
+        num_insertions = bound_diff-1
+        max_node = max(list(B.nodes())) + 1 
+        new_nodes = [start_node] + list(range(max_node + 1, max_node + num_insertions)) + [end_node]
+        new_edges = [[new_nodes[i], new_nodes[i+1]] for i in range(len(new_nodes)-1)]
+
+        #Update positions for new nodes
+        new_height = [np.mean(interval_points[start_bound_idx-1 + i : start_bound_idx + i ]) for i in range(bound_diff+1)] 
+        new_x = np.linspace(B.nodes[start_node]['Position'][0], B.nodes[end_node]['Position'][0], bound_diff+1)
+        new_y = np.linspace(B.nodes[start_node]['Position'][1], B.nodes[end_node]['Position'][1], bound_diff+1)
+
+        new_attributes = {new_nodes[idx] : {'Position' : [new_x[idx], new_y[idx],new_height[idx]], 'Visited' : 1, 'Merged':[],'Inserted':[]} for idx in range(1,len(new_nodes)-1)}
+
+        #Insert connected nodes at center of interval
+        B.add_edges_from(new_edges)
+        nx.set_node_attributes(B,new_attributes)
+
+        for node_idx in range(len(new_nodes)):
+            B.node[new_nodes[node_idx]]['Inserted'].append(new_nodes)
+     
+
+        return None
+
+#Recursive function that goes through all edges 
+def graph_search(A, start, interval_points):
+    if A.nodes[start]['Visited'] == 1:
+        return
+    else:
+        A.nodes[start]['Visited'] = 1
+        neighbors = list(A.neighbors(start))
+
+        #Insert or remove nodes based on interval position
+        for neighbor_id in neighbors:
+            #If an edge was merged, the neighbor list updates to reflect new neighbors
+            neighbor_update = edge_edit(A, start, neighbor_id, interval_points)
+            if neighbor_update != None:
+                neighbors.extend(neighbor_update)
+                continue #Go on to next neighbor
+
+            else:
+                graph_search(A,neighbor_id,interval_points)
+            
+
+        
+        return
+
+
+   
